38ce972b7b3e848fc722e747ef8d45e74a246ca6
Balaji Sivasakthi <65065614+balaji-sivasakthi@users.noreply.github.com>
2023-03-21 19:28:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const files = fs.readdirSync(__dirname+"/views")
 files.forEach(file=>{
-    if(fs.readdirSync('.html')){
+    if(file.includes('.html')){
         let oldFileName = file.split('.')[0];
         fs.renameSync(`${__dirname}/views/${oldFileName}.html`,`${__dirname}/views/${oldFileName}.ejs`)
     }
