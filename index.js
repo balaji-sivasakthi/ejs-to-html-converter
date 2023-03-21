a54ee08cdcd74f8f49b2125101dbea0cc87ef24c
balaji-sivasakthi <balajisivasakthi2020@gmail.com>
2023-03-21 19:25:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const files = fs.readdirSync(__dirname+"/views")
+files.forEach(file=>{
+    if(fs.readdirSync('.html')){
+        let oldFileName = file.split('.')[0];
+        fs.renameSync(`${__dirname}/views/${oldFileName}.html`,`${__dirname}/views/${oldFileName}.ejs`)
+    }
+})
